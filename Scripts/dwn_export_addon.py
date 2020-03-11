1c5c3958c5ebe7e422e39a10834b9b913c218a3d
Andrew Dawson <dawso.andrew@gmail.com>
2020-03-10 21:26:10 -0400

    Begins adding file writing.

--- Scripts/dwn_export_addon.py
+++ Scripts/dwn_export_addon.py
@@ -1,4 +1,6 @@
 import bpy
+from bpy_extras.io_utils import ExportHelper
+from bpy.types import Operator
 
 bl_info = {
     "author": "Andrew Dawson <dawso.andrew@gmail.com>",
@@ -10,30 +12,64 @@ bl_info = {
     "version": (1, 0, 0),
 }
 
+file_header_tag = "DWNSCENE"
+file_version = 1
 
-class OBJECT_OT_export_dwn(bpy.types.Operator):
+
+class OBJECT_OT_export_dwn(Operator, ExportHelper):
     """.dwn file export addon"""
     bl_idname = "object.export_dwn"
     bl_label = "Dawn Toolbox Export"
     bl_options = {"REGISTER", "UNDO"}
-    filename_ext = ".dwn"
 
-    filepath = bpy.props.StringProperty(subtype="FILE_PATH")
+    filename_ext = ".dwn"
     filter_glob: bpy.props.StringProperty(
         default="*.dwn", options={"HIDDEN"}, maxlen=255)
-    use_setting: bpy.props.BoolProperty(
+
+    should_export_selected_only: bpy.props.BoolProperty(
         name="Selected only", description="Export selected mesh items only",
         default=True)
 
     def execute(self, context):
         if context.active_object.mode == "EDIT":
             bpy.ops.object.mode_set(mode="OBJECT")
-        print("Howdy!")
+        objects = (context.selected_objects
+                   if self.should_export_selected_only else
+                   context.scene.objects)
+        save_dwn(objects, self.filepath)
         return {"FINISHED"}
 
-    def invoke(self, context, event):
-        context.window_manager.fileselect_add(self)
-        return {"RUNNING_MODAL"}
+
+def write_string(file, value):
+    value_bytes = value.encode("utf-8")
+    file.write(value_bytes)
+
+
+def write_uint16(file, value):
+    uint16_value = value.to_bytes(2, byteorder="little", signed=False)
+    file.write(uint16_value)
+
+
+def write_uint32(file, value):
+    uint32_value = value.to_bytes(4, byteorder="little", signed=False)
+    file.write(uint32_value)
+
+
+def write_uint8(file, value):
+    uint8_value = value.to_bytes(1, byteorder="little", signed=False)
+    file.write(uint8_value)
+
+
+def write_file_header(file, byte_count):
+    write_string(file, file_header_tag)
+    write_uint32(file, file_version)
+    write_uint32(file, byte_count)
+
+
+def save_dwn(objects, filepath):
+    with open(filepath, "wb") as file:
+        write_file_header(file, 1)
+        write_uint8(file, 7)
 
 
 def export_menu_item_func(self, context):
